e157d1f20af09806e4a979d612c756aafb48a668
Henrique Souza <henrique.fskt@gmail.com>
2026-07-30 20:43:43 -0300

    refactor: move database credentials to environment variables

--- database/database.go
+++ database/database.go
@@ -2,15 +2,29 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 var DB *sql.DB
 
 func Connect() {
-	connStr := "postgres://financeapi:financeapi123@localhost:5432/financedb?sslmode=disable"
+	if err := godotenv.Load(); err != nil {
+		log.Println("aviso: arquivo .env não encontrado, usando variáveis de ambiente do sistema")
+	}
+
+	connStr := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
 
 	db, err := sql.Open("pgx", connStr)
 	if err != nil {
