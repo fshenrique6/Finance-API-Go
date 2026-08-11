e2bde8cffd4e5fe86f1964fbe4f0f2fbd0c3403b
Henrique Souza <henrique.fskt@gmail.com>
2026-08-11 10:10:34 -0300

    feat: added user table & dependencies

--- init.sql
+++ init.sql
@@ -1,3 +1,11 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
 CREATE TABLE transactions (
     id SERIAL PRIMARY KEY,
     description TEXT NOT NULL,
