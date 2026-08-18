d74445fbc40f16a062d6af2145bdfee89f8ceb64
Henrique Souza <henrique.fskt@gmail.com>
2026-08-18 10:54:53 -0300

    bufix: removed invalid space in struct tags

--- models/user.go
+++ models/user.go
@@ -6,7 +6,7 @@ type User struct {
 	ID		   int       `json:"id"`
 	Name       string    `json:"name" binding:"required"`
 	Email      string    `json:"email" binding:"required,email"`
-	Password   string    `json:"password, omitempty" binding:"required, min=6"`
+	Password   string    `json:"password,omitempty" binding:"required,min=6"`
 	PasswordHash string    `json:"-"`
 	CreatedAt  time.Time `json:"created_at"`
 }
